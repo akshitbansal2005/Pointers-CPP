e9ecd750d82f814b6ea64e4cd06578ea37be65d4
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2024-09-25 12:42:51 +0530

    Create increment_pointer.cpp

---
+++ increment_pointer.cpp
@@ -0,0 +1,18 @@
+//Akshit Bansal
+//23070123013
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int*aptr=&a;
+    cout<<aptr<<endl;
+    
+    aptr++;
+    cout<<aptr<<endl;
+
+    return 0;
+}
+
+0x7ffc8e57b444
+0x7ffc8e57b448
